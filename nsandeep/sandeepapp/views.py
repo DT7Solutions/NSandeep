7ae8565955bd92c65364264e9ce20d3fa41615cd
DT7Solutions <developer@dt7solutions.com>
2023-01-04 12:50:40 +0530

    second instalation for django

--- nsandeep/sandeepapp/views.py
+++ nsandeep/sandeepapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Create your views here.
+def home(request):
+    return render(request, 'uifiles/index.html')
+
+
+def page_not_found_view(request, exception):
+    return render(request, 'uifiles/404.html', status=404)
