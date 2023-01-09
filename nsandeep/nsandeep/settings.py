746957355f5fc8a7ef7e30e2a98849b1b7f9503f
DT7Solutions <developer@dt7solutions.com>
2023-01-09 15:48:48 +0530

    adding 404page

--- nsandeep/nsandeep/settings.py
+++ nsandeep/nsandeep/settings.py
@@ -26,9 +26,9 @@ STATIC_DIR =os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'django-insecure-d+pc32cd8_qxq338#9lv6gk@=)tuh%il8r-2@6*1p-kq3_j0w('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['3.110.189.209']
 
 
 # Application definition
--- nsandeep/nsandeep/urls.py
+++ nsandeep/nsandeep/urls.py
@@ -23,3 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',  include('sandeepapp.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.UPLOAD_URL, document_root=settings.UPLOAD_URL)
+
+handler404 = "sandeepapp.views.page_not_found_view"
